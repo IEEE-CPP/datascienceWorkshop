aee1e3cfd651f3d19a0aef9d7a531be614160f17
drnfc <djbatfrog@gmail.com>
2025-02-18 12:14:50 -0800

    feat(explore): added more stuff to explore

--- lib/explore.py
+++ lib/explore.py
@@ -4,7 +4,11 @@ from pandas import DataFrame
 def explore(data: DataFrame):
     # get first few rows
     print(data.head())
-    # get summary
+    # get last few rows
+    print(data.tail())
+    # get summary, data types, that sort of thing
     print(data.info())
     # get descriptive statistics
     print(data.describe())
+    # get number of nuls
+    print(data.isnull().sum())
