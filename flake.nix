c62ca58ce7a1a62a294ef8eaf3474ae9a913709f
drnfc <djbatfrog@gmail.com>
2025-02-20 15:17:40 -0800

    feat(flake): support python venvs better

--- flake.nix
+++ flake.nix
@@ -10,25 +10,38 @@
     let
       system = "x86_64-linux";
       pkgs = inputs.nixpkgs.legacyPackages.${system};
+      env =
+        script:
+        (pkgs.buildFHSEnv {
+          name = "python-env";
+          targetPkgs =
+            pkgs:
+            (with pkgs; [
+              python313
+              python313Packages.pip
+              python313Packages.virtualenv
+              # Support binary wheels from PyPI
+              pythonManylinuxPackages.manylinux2014Package
+              # Enable building from sdists
+              cmake
+              ninja
+              gcc
+              pre-commit
+            ]);
+          runScript = "${
+            pkgs.writeShellScriptBin "runScript" (
+              ''
+                    set -e
+                    test -d env || ${pkgs.python313.interpreter} -m venv env
+                source env/bin/activate
+                set +e
+              ''
+              + script
+            )
+          }/bin/runScript";
+        }).env;
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          zsh
-          basedpyright
-          (python313FreeThreading.withPackages (p: [
-            p.jupyter
-            p.jupytext
-            p.ipython
-            p.pandas
-            p.matplotlib
-            p.seaborn
-          ]))
-        ];
-        shellHook = ''
-          zsh
-          exit
-        '';
-      };
+      devShells.${system}.default = env "zsh";
     };
 }
